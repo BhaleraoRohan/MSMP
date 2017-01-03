3dfbc00579a6533ccb847e4b1df0881b05e1ced1
Rohan Bhalerao <bhalerao.rohan28@gmail.com>
2017-01-03 10:17:26 +0530

    Git setup test

---
+++ src/sitemasterData/SitemasterData.java
@@ -0,0 +1,29 @@
+package sitemasterData;
+
+public class SitemasterData {
+	
+	
+	int id; String siteId; String sitename; String fscname; String circle; String zone; String cluster;String district;
+
+
+
+
+
+	public SitemasterData(int id, String siteId, String sitename, String fscname, String circle, String zone,
+			String cluster, String district) {
+		super();
+		this.id = id;
+		this.siteId = siteId;
+		this.sitename = sitename;
+		this.fscname = fscname;
+		this.circle = circle;
+		this.zone = zone;
+		this.cluster = cluster;
+		this.district = district;
+	}
+
+
+
+	
+
+}
